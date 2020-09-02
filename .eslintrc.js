f26698dc32ec4f94da1f29ee9df46664a4a3f7e9
NhanNguyen001 <nhan2151@gmail.com>
2020-09-02 17:43:41 +0700

    Setup ESlint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'import/prefer-default-export': 'off',
+    'func-names': 'off',
+  },
+};
